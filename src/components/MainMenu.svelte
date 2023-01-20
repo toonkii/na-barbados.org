c7f73c91f854f22b5032cbeef1c277cd19a778f2
Thomas Clarke <thomas.p.clarke@gmail.com>
2023-01-20 12:46:06 -0400

    Additional routes.

--- src/components/MainMenu.svelte
+++ src/components/MainMenu.svelte
@@ -8,11 +8,17 @@
 		<ul>
 			<li><a href="/">Home</a></li>
 			<li><a href="/meetings">Meetings</a></li>
+			<li><a class="btn-background" href="/convention" role="button">2023 Convention</a></li>
 			<li><a href="/about">About</a></li>
-			<!-- <li><a href="#" role="button">Button</a></li> -->
+			<li><a href="/contact">Contact Us</a></li>
 		</ul>
 	</nav>
 </main>
 
 <style>
+.btn-background {
+	color: #000;
+	background-color: red;
+	border: 0px;
+}	
 </style>
