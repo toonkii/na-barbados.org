d2f546062d96e44f8e1d75f21cebf5a2db7cc8f9
Thomas Clarke <thomas.p.clarke@gmail.com>
2023-01-20 13:11:33 -0400

    Normal menu item.

--- src/components/MainMenu.svelte
+++ src/components/MainMenu.svelte
@@ -8,7 +8,8 @@
 		<ul>
 			<li><a href="/">Home</a></li>
 			<li><a href="/meetings">Meetings</a></li>
-			<li><a class="btn-background" href="/convention" role="button">2023 Convention</a></li>
+			<!-- <li><a class="btn-background" href="/convention" role="button">2023 Convention</a></li> -->
+			<li><a href="/convention">2023 Convention</a></li>
 			<li><a href="/about">About</a></li>
 			<li><a href="/contact">Contact Us</a></li>
 		</ul>
