446c211ac28f51687bc6fb441973ab836d2c195b
Thomas Clarke <thomas.p.clarke@gmail.com>
2023-01-21 09:59:07 -0400

    Hero with image.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,35 @@
-<h1>Front Page</h1>
+<script>
+	let src = '/tianhao-wang-iEIcxy2HmKo-unsplash.jpg';
+</script>
+
+<main class="container">
+	<div class="container-fluid hero">
+		<header>
+			<hgroup>
+				<h1>Front Page</h1>
+				<h2>Sub heading</h2>
+				<p class="banner-text">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum doloremque iste, laudantium
+					neque consectetur totam impedit, nisi ipsa quo, corporis deleniti explicabo molestiae
+					quisquam excepturi ab animi aliquam distinctio delectus! Ut ducimus atque voluptatum?
+					Minima excepturi, voluptatem assumenda perferendis numquam distinctio possimus alias
+					similique consequuntur sint, aperiam voluptas. Ducimus, sequi.
+				</p>
+			</hgroup>
+			<!-- <p><a href="#" role="button">Call to action</a></p> -->
+		</header>
+	</div>
+</main>
+
+<style>
+	.hero {
+		background-color: #394046;
+		background-image: url('/tianhao-wang-iEIcxy2HmKo-unsplash.jpg');
+		background-position: center;
+		background-size: cover;
+	}
+
+    .banner-text {
+        color: black;
+    }
+</style>
