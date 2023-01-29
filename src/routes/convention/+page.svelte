63f256f26a733a677b16a2e4ba9655c341d69e7a
Thomas Clarke <thomas.p.clarke@gmail.com>
2023-01-29 09:27:59 -0400

    Convention information layout.

--- src/routes/convention/+page.svelte
+++ src/routes/convention/+page.svelte
@@ -1,1 +1,23 @@
 <h1>2023 Convention Information</h1>
+
+<img src="/maarten-deckers-T5nXYXCf50I-unsplash-1500x750.jpg" alt="Architecture" />
+
+<p>
+	Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis illum beatae nihil
+	consectetur maxime nemo, molestias iure laborum. Illo quia perspiciatis, nam tempore soluta itaque
+	provident repudiandae asperiores sint autem. Qui adipisci quisquam praesentium illo natus eveniet
+	culpa in excepturi blanditiis! Tempora delectus corrupti, deleniti accusamus reiciendis eius,
+	exercitationem ex, laudantium repudiandae ducimus doloribus hic officiis consequuntur laboriosam
+	animi quae repellendus! Rem velit quasi omnis. Aut necessitatibus iste laboriosam itaque ipsum
+	reiciendis. Natus similique iste amet vitae? Repellat numquam vero facilis laudantium, nobis
+	architecto! Numquam reprehenderit corporis esse quibusdam vitae et asperiores officiis temporibus
+	aliquam eveniet reiciendis culpa expedita, ea, magni alias, consequuntur nihil magnam! Magni
+	placeat facere veniam, sunt ab dolorem quibusdam dicta dolore, architecto earum assumenda
+	cupiditate alias a nostrum incidunt vero, ipsum molestias consequuntur recusandae commodi iusto
+	amet consequatur deleniti! Possimus assumenda corporis delectus veritatis quae a, qui, id quod,
+	consequuntur molestias dolore recusandae perspiciatis distinctio officia mollitia aliquam eos
+	maiores est unde reprehenderit voluptate similique deleniti corrupti? In enim deleniti, adipisci
+	esse dolor doloremque et, non dolorem, sed aliquam aspernatur beatae dicta voluptatum nulla sint
+	quo ullam ducimus autem commodi a! Nulla fugit impedit facilis voluptatem, ut maxime quos
+	similique ratione officiis voluptates. Saepe, voluptas porro.
+</p>
