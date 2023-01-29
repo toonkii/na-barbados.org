c72211e97604906656a7c586d7b4440dadfa3047
Thomas Clarke <thomas.p.clarke@gmail.com>
2023-01-29 09:27:00 -0400

    Rudimentary email form.

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,1 +1,23 @@
 <h1>Contact Us</h1>
+
+<main class="container">
+    <p>Fill in the form below and send us an email.</p>
+    <p>We will get back to you as soon as possible.</p>
+    
+    <form action="submit">
+        <label for="name">Name</label>
+        <input id="name" type="text">
+
+        <label for="email">Your Email Address</label>
+        <input id="email" type="email">
+
+        <label for="subject">Subject</label>
+        <input id="subject" type="text">
+        
+        <label for="body">Email Body</label>
+        <textarea id="body" rows="5"></textarea>
+
+        <a href="#" role="button">Send</a>
+        <a href="#" role="button" class="secondary">Cancel</a>
+    </form>
+</main>
