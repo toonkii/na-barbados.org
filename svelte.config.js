2210f06b6f2f0e72f2a5e271e07968ce7142155e
Thomas Clarke <thomas.p.clarke@gmail.com>
2023-02-05 10:26:09 -0400

    Playing with adapters. Settling on adapter-node

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,48 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+//////
+// Node adapter.
+//
+import adapter from '@sveltejs/adapter-node';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
+export default {
 	kit: {
-		adapter: adapter()
+	  adapter: adapter()
 	}
-};
+  };
 
-export default config;
+/////
+// Auto adapter.
+//
+// import adapter from '@sveltejs/adapter-auto';
+// import { vitePreprocess } from '@sveltejs/kit/vite';
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+// 	// for more information about preprocessors
+// 	preprocess: vitePreprocess(),
+
+// 	kit: {
+// 		adapter: adapter()
+// 	}
+// };
+
+// export default config;
+
+/////
+// Static adapter.
+//
+// import adapter from '@sveltejs/adapter-static';
+
+// export default {
+// 	kit: {
+// 	  adapter: adapter({
+// 		// default options are shown. On some platforms
+// 		// these options are set automatically — see below
+// 		pages: 'build',
+// 		assets: 'build',
+// 		fallback: 'index.html',
+// 		precompress: false,
+// 		strict: true
+// 	  })
+// 	}
+//   };
